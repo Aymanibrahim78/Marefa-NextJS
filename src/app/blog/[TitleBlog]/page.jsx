25298cdc1ed20e93522a3e66d49ed1ead269aeac
Ayman Ibrahim <110949544+Aymanibrahim78@users.noreply.github.com>
2024-06-15 00:40:55 +0300

    page.jsx

--- src/app/blog/[TitleBlog]/page.jsx
+++ src/app/blog/[TitleBlog]/page.jsx
@@ -8,7 +8,7 @@ import NotFound from '@/app/not-found';
 
 
 async function getData() {
-    await new Promise(resolve => setTimeout(resolve, 6000))
+    // await new Promise(resolve => setTimeout(resolve, 6000))
   
     const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}blogs?populate=*`);
   
